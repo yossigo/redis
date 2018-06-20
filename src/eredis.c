4a9dab8c69ca11db9f151a69141e6786f7db0a8c
Yossi Gottlieb <yossigo@gmail.com>
2018-06-20 20:57:18 +0300

    Add some eredis interface code.

--- src/eredis.c
+++ src/eredis.c
@@ -30,26 +30,28 @@ eredis_client_t *eredis_create_client(void)
     return (eredis_client_t *) c;
 }
 
-static void prepare_client(client *c, int args_count, const char **args)
-{
-    int i;
-    if (c->argv) {
-        for (i = 0; i < c->argc; i++) decrRefCount(c->argv[i]);
-        zfree(c->argv);
-    }
-
-    c->argv = zmalloc(sizeof(robj *) * args_count);
-    c->argc = args_count;
-    for (i = 0; i < c->argc; i++) {
-        c->argv[i] = createStringObject(args[i], strlen(args[i]));
-    }
-    c->bufpos = 0;
-}
-
-const char *eredis_execute(eredis_client_t *c, int args_count, const char **args, int *reply_len)
+int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args)
 {
     client *rc = (client *) c;
-    prepare_client(rc, args_count, args);
+    int i;
+    if (rc->argv) {
+        for (i = 0; i < rc->argc; i++) decrRefCount(rc->argv[i]);
+        zfree(rc->argv);
+    }
+
+    rc->argv = zmalloc(sizeof(robj *) * args_count);
+    rc->argc = args_count;
+    for (i = 0; i < rc->argc; i++) {
+        rc->argv[i] = createStringObject(args[i], strlen(args[i]));
+    }
+    rc->bufpos = 0;
+
+    return 0;
+}
+
+const char *eredis_execute(eredis_client_t *c, int *reply_len)
+{
+    client *rc = (client *) c;
     if (processCommand(rc) != C_OK) return NULL;
 
     *reply_len = rc->bufpos;
--- src/eredis.h
+++ src/eredis.h
@@ -5,7 +5,8 @@ typedef struct eredis_client eredis_client_t;
 
 int eredis_init(void);
 eredis_client_t *eredis_create_client(void);
-const char *eredis_execute(eredis_client_t *c, int args_count, const char **args, int *reply_len);
+int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args);
+const char *eredis_execute(eredis_client_t *c, int *reply_len);
 
 
 #endif  /* _EREDIS_H */
