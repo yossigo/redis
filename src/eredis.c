ebcf2000ad08156a1bd47fd114d9313cebc9bb69
Yossi Gottlieb <yossigo@gmail.com>
2018-06-25 23:08:32 +0300

    Various POC improvements.
    * Clients can be freed.
    * Support binary safe arguments.
    * Support long replies (>16K basic reply chunk buf).

--- src/eredis.c
+++ src/eredis.c
@@ -23,16 +23,30 @@ int eredis_init(void) {
     return 0;
 }
 
+struct eredis_client {
+    client *client;
+    size_t reply_bytes_read;
+    listIter reply_iter;
+};
+
 eredis_client_t *eredis_create_client(void)
 {
-    client *c = createClient(-1);
-    c->flags |= CLIENT_MODULE;      /* So we get replies even with fd == -1 */
-    return (eredis_client_t *) c;
+    eredis_client_t *c = zmalloc(sizeof(eredis_client_t));
+    c->client = createClient(-1);
+    c->client->flags |= CLIENT_MODULE;      /* So we get replies even with fd == -1 */
+    return c;
 }
 
-int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args)
+void eredis_free_client(eredis_client_t *c)
 {
-    client *rc = (client *) c;
+    freeClient(c->client);
+    zfree(c);
+}
+
+
+int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args, size_t *arg_lens)
+{
+    client *rc = c->client;
     int i;
     if (rc->argv) {
         for (i = 0; i < rc->argc; i++) decrRefCount(rc->argv[i]);
@@ -42,19 +56,39 @@ int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args
     rc->argv = zmalloc(sizeof(robj *) * args_count);
     rc->argc = args_count;
     for (i = 0; i < rc->argc; i++) {
-        rc->argv[i] = createStringObject(args[i], strlen(args[i]));
+        size_t len = arg_lens ? arg_lens[i] : strlen(args[i]);
+        rc->argv[i] = createStringObject(args[i], len);
     }
     rc->bufpos = 0;
+    c->reply_bytes_read = 0;
 
     return 0;
 }
 
-const char *eredis_execute(eredis_client_t *c, int *reply_len)
+int eredis_execute(eredis_client_t *c)
 {
-    client *rc = (client *) c;
-    if (processCommand(rc) != C_OK) return NULL;
+    client *rc = c->client;
+    if (processCommand(rc) != C_OK) return -1;
 
-    *reply_len = rc->bufpos;
-    return rc->buf;
+    return 0;
+}
+
+const char *eredis_read_reply_chunk(eredis_client_t *c, int *chunk_len)
+{
+    if (!c->reply_bytes_read) {
+        listRewind(c->client->reply, &c->reply_iter);
+        c->reply_bytes_read = c->client->bufpos;
+        *chunk_len = c->client->bufpos;
+        return c->client->buf;
+    } else {
+        listNode *ln = listNext(&c->reply_iter);
+        if (ln) {
+            sds val = listNodeValue(ln);
+            *chunk_len = sdslen(val);
+            return val;
+        } else {
+            return NULL;
+        }
+    }
 }
 
--- src/eredis.h
+++ src/eredis.h
@@ -3,10 +3,29 @@
 
 typedef struct eredis_client eredis_client_t;
 
+/* Initialize the eredis library - must be done once per process! */
 int eredis_init(void);
+
+/* Client create/free */
 eredis_client_t *eredis_create_client(void);
-int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args);
-const char *eredis_execute(eredis_client_t *c, int *reply_len);
+void eredis_free_client(eredis_client_t *c);
+
+/* Prepare a request, before calling execute. If arg_lens is NULL, strings are
+ * processed as null terminated. Otherwise lengths are taken from the separate
+ * size_t array.
+ */
+int eredis_prepare_request(eredis_client_t *c, int args_count, const char **args, size_t *arg_lens);
+
+/* Execute request; returns 0 on success. */
+int eredis_execute(eredis_client_t *c);
+
+/* Read chunk from reply. Chunking depends on internal Redis representation
+ * so make no assumptions.
+ *
+ * Every call returns a pointer and updates chunk_len. When no more chunks
+ * are available, NULL is returned.
+ */
+const char *eredis_read_reply_chunk(eredis_client_t *c, int *chunk_len);
 
 
 #endif  /* _EREDIS_H */
